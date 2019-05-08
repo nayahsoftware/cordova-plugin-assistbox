74bf64c39870ee17165b6cd5f7a58960b781000f
YILDIRAY YILMAZ <yildiray.yilmaz@nayahsoftware.com>
2019-05-08 18:18:29 +0300

    ...

--- src/ios/AssistBox.m
+++ src/ios/AssistBox.m
@@ -14,12 +14,6 @@
 	NSString *tokenArg = [command argumentAtIndex:0];
     if(tokenArg != nil){
 		vc.token = tokenArg;
-	
-		//canli
-		//vc.token = @"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb21wYW55SWQiOjEsImNsYWltRmlsZU5vIjoiMTI5MTI5MTI5IiwiYXBwb2ludG1lbnRJZCI6OTA3LCJmdWxsTmFtZSI6IllJTERJUkFZIFlJTE1BWiIsInVzZXJSb2xlIjoiR1VFU1QiLCJleHAiOjE1NjYyMzExMjMsInBob25lTm8iOiIrOTA1MzIzNjU2NjY5In0._sc1iMcrGoH07QvwuHHj5gGBhJCSi7UoPF2NqWCePA8";
-    
-		//test - appointment 516
-		//vc.token =@"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjb21wYW55SWQiOjEsImNsYWltRmlsZU5vIjoiMTkyMTY4MTk5OCIsImFwcG9pbnRtZW50SWQiOjUxNiwiZnVsbE5hbWUiOiJZxLFsZMSxcmF5IFnEsWxtYXo1IiwidXNlclJvbGUiOiJHVUVTVCIsImV4cCI6MTU2NjI0NTc0NywicGhvbmVObyI6Iis5MDUzMjM2NTY2NjkifQ.j6C1mf6W5HNCCEUSe8SChdZPzOt_kL2KVRrM6NBn5c0";
 		//UINavigationController* navController = getNavigationControllerFromVC(owner);
 		UIWindow *window = [[UIApplication sharedApplication] keyWindow];
 		vc.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
@@ -28,7 +22,7 @@
 		[navController presentViewController:vc animated:YES completion:nil];
 	} else {
 		CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Token is required!"];
-		[self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
+		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 	}
 }
 @end
