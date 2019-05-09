3ec34febbc3daa37f7caa84068981c25b339ff40
yildirayyilmaz <yildiray.yilmaz@hotmail.com>
2019-05-09 12:20:32 +0300

    ..

--- www/AssistBox.js
+++ www/AssistBox.js
@@ -1,9 +1,8 @@
-cordova.define("cordova-plugin-assistbox.AssistBox", function(require, exports, module) {
-  var exec = require("cordova/exec");
-  function AssistBox() {};
+var exec = require("cordova/exec");
+function AssistBox() {};
   
-  AssistBox.prototype.goToAssistBox = function(successCallback, errorCallback,args) {
-    exec(successCallback, errorCallback, "AssistBox", "goToAssistBox", args);
-  };
-  module.exports = new AssistBox();
-});
+AssistBox.prototype.goToAssistBox = function(successCallback, errorCallback,args) {
+	exec(successCallback, errorCallback, "AssistBox", "goToAssistBox", args);
+};
+module.exports = new AssistBox();
+
