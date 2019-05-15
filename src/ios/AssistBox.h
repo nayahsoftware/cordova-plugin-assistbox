5e3cfacfffd2588b9b2cb260cd2467d2efbb1bb9
YILDIRAY YILMAZ <yildiray.yilmaz@nayahsoftware.com>
2019-05-15 09:54:38 +0300

    If target is simulator, remove AssistBox framework references for main app initialization

--- src/ios/AssistBox.h
+++ src/ios/AssistBox.h
@@ -1,7 +1,13 @@
+#if TARGET_IPHONE_SIMULATOR
+#import <AVFoundation/AVFoundation.h>
+#import <Cordova/CDV.h>
+#import <Cordova/CDVPlugin.h>
+#else
 #import <AVFoundation/AVFoundation.h>
 #import <Cordova/CDV.h>
 #import <AssistBoxLib/AssistBoxViewController.h>
 #import <Cordova/CDVPlugin.h>
+#endif
 
 @interface AssistBox : CDVPlugin
 - (void) goToAssistBox:(CDVInvokedUrlCommand*)command;
--- src/ios/AssistBox.m
+++ src/ios/AssistBox.m
@@ -6,23 +6,27 @@
     
 }
 
-- (void) goToAssistBox:(CDVInvokedUrlCommand*)command {    
+- (void) goToAssistBox:(CDVInvokedUrlCommand*)command {
+#if TARGET_IPHONE_SIMULATOR
+    
+#else
     NSBundle *frameworkBundle = [NSBundle bundleForClass:[AssistBoxViewController class]];
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"AssistBox" bundle:frameworkBundle];
     AssistBoxViewController *vc = [storyboard instantiateViewControllerWithIdentifier:@"assistBoxViewController"];
-	
-	NSString *tokenArg = [command argumentAtIndex:0];
+    
+    NSString *tokenArg = [command argumentAtIndex:0];
     if(tokenArg != nil){
-		vc.token = tokenArg;
-		//UINavigationController* navController = getNavigationControllerFromVC(owner);
-		UIWindow *window = [[UIApplication sharedApplication] keyWindow];
-		vc.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
-
-		UIViewController* navController = (UIViewController*)window.rootViewController;
-		[navController presentViewController:vc animated:YES completion:nil];
-	} else {
-		CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Token is required!"];
-		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-	}
+        vc.token = tokenArg;
+        //UINavigationController* navController = getNavigationControllerFromVC(owner);
+        UIWindow *window = [[UIApplication sharedApplication] keyWindow];
+        vc.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+        
+        UIViewController* navController = (UIViewController*)window.rootViewController;
+        [navController presentViewController:vc animated:YES completion:nil];
+    } else {
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Token is required!"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }
+#endif
 }
 @end
