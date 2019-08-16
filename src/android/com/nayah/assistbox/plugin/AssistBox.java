ff2f114b8687864db4beac005a63de48170d6ac0
yildirayyilmaz <yildiray.yilmaz@hotmail.com>
2019-08-16 13:47:37 +0300

    source code reformat

--- src/android/com/nayah/assistbox/plugin/AssistBox.java
+++ src/android/com/nayah/assistbox/plugin/AssistBox.java
@@ -12,31 +12,31 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class AssistBox extends CordovaPlugin {
-  private static final String LOG_TAG = "AssistBox";
+    private static final String LOG_TAG = "AssistBox";
 
-  private static final String ACTION_GO_TO_ASSISTBOX = "goToAssistBox";
-  
-  public AssistBox() {
-  
-  }
+    private static final String ACTION_GO_TO_ASSISTBOX = "goToAssistBox";
 
-  @Override
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    try {
-      if (ACTION_GO_TO_ASSISTBOX.equals(action)) {
-        Context context = cordova.getActivity().getApplicationContext();
-        Intent intent = new Intent(context, AssistBoxActivity.class);
-        cordova.getActivity().startActivity(intent);
-		callbackContext.success("redirected");
-        return true;
-      } else {
-        callbackContext.error("AssistBox plugin error." + action + " is not a supported function. Did you mean '" + ACTION_GO_TO_ASSISTBOX + "'?");
-        return false;
-      }
-    } catch (Exception e) {
-      callbackContext.error(e.getMessage());
-      return false;
+    public AssistBox() {
+
     }
-  }
+
+    @Override
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        try {
+            if (ACTION_GO_TO_ASSISTBOX.equals(action)) {
+                Context context = cordova.getActivity().getApplicationContext();
+                Intent intent = new Intent(context, AssistBoxActivity.class);
+                cordova.getActivity().startActivity(intent);
+                callbackContext.success("redirected");
+                return true;
+            } else {
+                callbackContext.error("AssistBox plugin error." + action + " is not a supported function. Did you mean '" + ACTION_GO_TO_ASSISTBOX + "'?");
+                return false;
+            }
+        } catch (Exception e) {
+            callbackContext.error(e.getMessage());
+            return false;
+        }
+    }
 
 }
